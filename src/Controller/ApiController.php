11b33ef715f9539653700d49e1c84fe13be06b63
Jim276 <jimmy.beaudoin98@gmail.com>
2022-05-04 17:30:11 +0200

    TP symfony API

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Region;
+use App\Entity\Departement;
+use App\Repository\CommuneRepository;
+use App\Repository\DepartementRepository;
 use App\Repository\RegionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,11 +14,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiController extends AbstractController
 {
     #[Route('/api/regions', name: 'get_regions_api')]
-    public function addRegionByApi(SerializerInterface $serializer, EntityManagerInterface $em): Response
+    public function addRegionsByApi(SerializerInterface $serializer, EntityManagerInterface $em): Response
     {
         //Récupération des régions en JSON
         $regionJSON = file_get_contents("https://geo.api.gouv.fr/regions");
@@ -28,7 +33,6 @@ class ApiController extends AbstractController
         //$em = $this->getDoctrine()->getManager();
         //dd($regionObject);
 
-
         foreach($regionObject as $region){
             $em->persist($region);
         }
@@ -37,25 +41,86 @@ class ApiController extends AbstractController
         return new JsonResponse("success", Response::HTTP_CREATED, [], true);
     }
 
+    #[Route('/api/departements', name: 'get_departements_api')]
+    public function addDepartementsByApi(SerializerInterface $serializer, EntityManagerInterface $em): Response
+    {
+        $departementJSON = file_get_contents("https://geo.api.gouv.fr/departements");
+        $departementObject = $serializer->deserialize($departementJSON, 'App\Entity\Departement[]', 'json');
+
+        foreach($departementObject as $departement){
+            $em->persist($departement);
+        }
+        $em->flush();
+
+        return new JsonResponse("success", Response::HTTP_CREATED, [], true);
+    }
+
+    #[Route('/api/communes', name: 'get_communes_api')]
+    public function addCommunesByApi(SerializerInterface $serializer, EntityManagerInterface $em): Response
+    {
+        $communeJSON = file_get_contents("https://geo.api.gouv.fr/communes");
+        $communeObject = $serializer->deserialize($communeJSON, 'App\Entity\Commune[]', 'json');
+
+        foreach($communeObject as $commune){
+            $em->persist($commune);
+        }
+        $em->flush();
+
+        return new JsonResponse("success", Response::HTTP_CREATED, [], true);
+    }
+
+
+
     #[Route('/api/show_regions', name: 'get_regions_api_BD')]
     public function showRegion(SerializerInterface $serializer, RegionRepository $regionRepository): Response
     {
         //Récupération des régions dans la BDD
         $regionObject = $regionRepository->findAll();
-
         //Serialize Object to JSON
         $regionJSON = $serializer->serialize($regionObject, "json");
-
         return new JsonResponse($regionJSON, Response::HTTP_OK, [], true);
     }
 
+    #[Route('/api/show_departements', name: 'get_departements_api_BD')]
+    public function showDepartement(SerializerInterface $serializer, DepartementRepository $departementRepository): Response
+    {
+        //Récupération des régions dans la BDD
+        $departementObject = $departementRepository->findAll();
+        //Serialize Object to JSON
+        $departementJSON = $serializer->serialize($departementObject, "json");
+        return new JsonResponse($departementJSON, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/show_communes', name: 'get_communes_api_BD')]
+    public function showCommune(SerializerInterface $serializer, CommuneRepository $communeRepository): Response
+    {
+        //Récupération des régions dans la BDD
+        $communeObject = $communeRepository->findAll();
+        //Serialize Object to JSON
+        $communeJSON = $serializer->serialize($communeObject, "json");
+        return new JsonResponse($communeJSON, Response::HTTP_OK, [], true);
+    }
+
+
+
     #[Route('/api/post_regions', name: 'post_regions_api_BD')]
-    public function addRegion(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    public function addRegion(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $em): Response
     {
         //Récupération du contenu du body
         $regionJSON = $request->getContent();
-        dd($regionJSON);
-        
+        $regionObject = $serializer->deserialize($regionJSON, Region::class, 'json');
+
+        $errors = $validator->validate($regionObject);
+
+        if(count($errors) > 0){
+            $errorsString = (string) $errors;
+            return new Response($errorsString);
+        }
+
+        $em->persist($regionObject);
+        $em->flush();
+        //dd($regionObject);
+        return new Response('Région valide');
     }
 }
 
